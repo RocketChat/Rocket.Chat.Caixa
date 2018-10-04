089907ddc48530801451532ebdc88e66a8cbf130
Renato Becker <renato.augusto.becker@gmail.com>
2018-10-04 17:37:12 -0300

    Improvements on OPTIONS method.

--- server.js
+++ server.js
@@ -75,10 +75,14 @@ router.get('/token/', verifyJWT, (req, res, next) => {
 });
 
 router.options('/token/', verifyJWT, (req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 	livechatProxy.authGuest(req.decoded, res);
+});
+
+app.options("/*", (req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+	next();
 });
 
 app.use('/api', router);
