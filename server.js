a93caf823c467def6fce50f9c3610bbe0c399ac8
Renato Becker <renato.augusto.becker@gmail.com>
2018-10-04 19:05:23 -0300

    Added `Access-Control-Allow-Credentials` header parameter.

--- server.js
+++ server.js
@@ -83,16 +83,19 @@ const allowCrossDomain = (req, res, next) => {
 
 	res.header('Access-Control-Allow-Origin', origin);
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE, OPTIONS');
+	res.header('Access-Control-Allow-Credentials', true);
 	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
 	/*
 	// intercept OPTIONS method
 	if ('OPTIONS' == req.method) {
-		res.send(200);
+		res.status(200).send();
 	}
 	else {
-		*/
+		next();
+	}*/
+
 	next();
-	//}
 };
 
 app.use(allowCrossDomain);
