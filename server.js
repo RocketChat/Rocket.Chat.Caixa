f5036547fcd2d8c24d4bab4d8359668157fa3f66
Renato Becker <renato.augusto.becker@gmail.com>
2018-10-04 17:25:37 -0300

    Added support to OPTIONS method.

--- server.js
+++ server.js
@@ -74,6 +74,13 @@ router.get('/token/', verifyJWT, (req, res, next) => {
 	livechatProxy.authGuest(req.decoded, res);
 });
 
+router.options('/token/', verifyJWT, (req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+	livechatProxy.authGuest(req.decoded, res);
+});
+
 app.use('/api', router);
 
 app.listen(port);
