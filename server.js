2a096b89f722f6cce0a771854def73b296126911
Renato Becker <renato.augusto.becker@gmail.com>
2018-10-04 18:47:20 -0300

    Added support to CORS.

--- server.js
+++ server.js
@@ -78,13 +78,24 @@ router.options('/token/', verifyJWT, (req, res, next) => {
 	livechatProxy.authGuest(req.decoded, res);
 });
 
-app.options("/*", (req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-	next();
-});
+const allowCrossDomain = (req, res, next) => {
+	const origin = req.headers.origin || req.headers.host;
 
+	res.header('Access-Control-Allow-Origin', origin);
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE, OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+	/*
+	// intercept OPTIONS method
+	if ('OPTIONS' == req.method) {
+		res.send(200);
+	}
+	else {
+		*/
+	next();
+	//}
+};
+
+app.use(allowCrossDomain);
 app.use('/api', router);
 
 app.listen(port);
